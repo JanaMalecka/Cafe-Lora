fb3fb56c74ffd2ec453eeb720669fe2203277b49
Jana Malecka <jm.jmalecka@gmail.com>
2021-05-01 14:20:39 +0200

    list of ingredients

--- src/Layer/index.js
+++ src/Layer/index.js
@@ -1,11 +1,21 @@
 import './style.css';
 
-export const Layer = (props) => {
+/* export const Layer = (props) => {
   return `
   <div class="layer">
     <div class="layer__color"
          style="background-color: ${props.color}"
           ></div>
     <div class="layer__label">${props.label}</div>
+          </div>`;
+}; */
+
+export const Layer = (layers) => {
+  return `
+  <div class="layer">
+    <div class="layer__color"
+         style="background-color: ${layers.color}"
+          ></div>
+    <div class="layer__label">${layers.label}</div>
           </div>`;
 };
