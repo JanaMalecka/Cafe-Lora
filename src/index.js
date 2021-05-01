40a6d77c14856d8accc7548ca9aa237a953aeed4
Jana Malecka <jm.jmalecka@gmail.com>
2021-05-01 11:44:16 +0200

    navigation disappears after click

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
 
 console.log('funguju!');
+
+const navBtnElm = document.querySelector('#nav-btn');
+const navElm = document.querySelector('nav');
+
+navBtnElm.addEventListener('click', () => {
+  navElm.classList.toggle('nav-closed');
+});
+
+const navLinkElm = document.querySelectorAll('.nav-link');
+
+for (let i = 0; i < navLinkElm.length; i++) {
+  navLinkElm[i].addEventListener('click', () => {
+    navElm.classList.add('nav-closed');
+  });
+}
